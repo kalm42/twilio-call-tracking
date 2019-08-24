9e5e35259c86e139c2ee4b1d6183b98ea5e26226
Kyle Melton <kalm42@gmail.com>
2019-08-24 17:52:38 -0600

    feat: Add firebase key and project id

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ module.exports = {
   TWILIO_SID: process.env.TWILIO_SID,
   TWILIO_AUTH: process.env.TWILIO_AUTH,
   PORT: process.env.PORT,
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
 }
